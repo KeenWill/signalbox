9425cbe0a4489ca1ea862efc240299812b923dda
William Goeller <william@williamgoeller.com>
2026-08-01 11:38:12 -0400

    Repair cost accounting compatibility gates

--- scripts/check_user_vocabulary.py
+++ scripts/check_user_vocabulary.py
@@ -19,7 +19,7 @@ OWNER_FRAGMENT = re.compile("owner", re.IGNORECASE)
 CROSS_FRAGMENT_OWNER = re.compile(r"(?:Unknown|Known)(?:Error|Rejection)")
 BARE_USER_MESSAGE = re.compile(r"(?i:\buser[ \t\r\n]+message\b)")
 REVIEWED_ALLOWLIST_SHA256 = (
-    "8984981505747a951e89b29a5e8005dc04caed29991e22275c1f850c9aec6a34"
+    "6849ec41381a0b1adc119119fe76b37270a464a96929f892deca491aed09fe4c"
 )
 
 
