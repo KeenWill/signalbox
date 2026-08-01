5ba789c4768537226b75bc03690d0da39936a3d9
William Goeller <william@williamgoeller.com>
2026-08-01 19:55:24 -0400

    Recompute delegation vocabulary inventory

--- scripts/check_user_vocabulary.py
+++ scripts/check_user_vocabulary.py
@@ -19,7 +19,7 @@ OWNER_FRAGMENT = re.compile("owner", re.IGNORECASE)
 CROSS_FRAGMENT_OWNER = re.compile(r"(?:Unknown|Known)(?:Error|Rejection)")
 BARE_USER_MESSAGE = re.compile(r"(?i:\buser[ \t\r\n]+message\b)")
 REVIEWED_ALLOWLIST_SHA256 = (
-    "972b814d9a109cd5bd04e8c30f676bb3ad8bae5470c22248d19e195254bfab76"
+    "17997e6a0e34c129e69a23ac6e4119337643c2dbfe54211ded88d6c351ff88eb"
 )
 
 
