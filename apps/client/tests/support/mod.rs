639c8a9886117c3778d24e5b5fbb71ae1e5135f0
William Goeller <william@williamgoeller.com>
2026-08-21 19:56:54 -0400

    Fix numeric bounds test fixture extraction

--- apps/client/tests/support/mod.rs
+++ apps/client/tests/support/mod.rs
@@ -8,7 +8,7 @@ pub fn parse_model_configuration(
         .split_once("[numeric_bounds]")
         .ok_or(HubModelConfigurationError::InvalidDocument)?;
     let (numeric_bounds, _) = numeric_bounds_and_after
-        .split_once("\n# Omit this table")
+        .split_once("\n[")
         .ok_or(HubModelConfigurationError::InvalidDocument)?;
     HubModelConfiguration::parse(&format!("{content}\n[numeric_bounds]{numeric_bounds}\n"))
 }
