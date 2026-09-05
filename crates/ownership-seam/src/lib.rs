91179ccb75e92ac9f63ea0752fc64535ac47ab37
William Goeller <william@williamgoeller.com>
2026-09-05 19:36:09 -0400

    Export the lifecycle event vocabulary

--- crates/ownership-seam/src/lib.rs
+++ crates/ownership-seam/src/lib.rs
@@ -5,15 +5,18 @@
 //! handles and the wider core outbox vocabulary do not cross this boundary.
 
 pub use signalbox_domain::{
-    ContextFrontierId, CreateSession, DeliveryRequest, DescendantTerminationScope,
-    DirectModelSelection, DurableCommandId, FinishCondition, FinishConditionStatement,
-    GoalGuidance, GoalStatement, GoalUserAction, GoalUserCommand, LifecycleActor, ModelCallId,
-    ModelSelectionOverride, ModelSelectionRequest, ModuleDispatch, PerInputConfigurationChoices,
-    RepoWatchDispatchId, SemanticTranscriptEntryId, SessionConfigurationDefaults,
-    SessionConfigurationDefaultsVersion, SessionCreationCause, SessionCreationProvenance,
-    SessionFailureCause, SessionId, SessionLifecycleCommand, SessionLifecycleOperation,
-    SessionLifecycleState, SessionOwnership, SessionOwnershipTransition, SessionTerminalOutcome,
-    StartGate, StopStickiness, SubmitInput, ToolAttemptId, TurnId, UserContent, UserContentPart,
+    ContextFrontierId, CoreAgency, CreateSession, DeliveryRequest, DescendantTerminationScope,
+    DirectModelSelection, DispatchingModule, DurableCommandId, FinishCondition,
+    FinishConditionStatement, GoalBlockedReasonKind, GoalGuidance, GoalStatement, GoalUserAction,
+    GoalUserCommand, LifecycleActor, ModelCallId, ModelSelectionOverride, ModelSelectionRequest,
+    ModuleDispatch, PerInputConfigurationChoices, RepoWatchDispatchId, SemanticTranscriptEntryId,
+    SessionConfigurationDefaults, SessionConfigurationDefaultsVersion, SessionCreationCause,
+    SessionCreationProvenance, SessionFailureCause, SessionId, SessionLifecycleCommand,
+    SessionLifecycleOperation, SessionLifecycleState, SessionOwnership, SessionOwnershipTransition,
+    SessionParkCause, SessionParkResponder, SessionRecoveryOperation, SessionRetirementCause,
+    SessionRetryableCause, SessionStructuralCause, SessionTerminalOutcome, SessionWait, StartGate,
+    StopStickiness, SubmitInput, ToolAttemptId, ToolRequestId, TurnId, UserContent,
+    UserContentPart,
 };
 pub use signalbox_persistence::outbox::OutboxDispatchError;
 use signalbox_persistence::outbox::{
---
+++ crates/ownership-seam/tests/lifecycle_vocabulary.rs
@@ -0,0 +1,26 @@
+use signalbox_ownership_seam::{
+    CoreAgency, DispatchingModule, GoalBlockedReasonKind, SessionFailureCause,
+    SessionLifecycleState, SessionParkCause, SessionParkResponder, SessionRecoveryOperation,
+    SessionRetirementCause, SessionRetryableCause, SessionStructuralCause, SessionTerminalOutcome,
+    SessionWait, ToolRequestId,
+};
+
+fn assert_nameable<T>() {}
+
+#[test]
+fn lifecycle_event_vocabulary_is_nameable_from_the_seam() {
+    assert_nameable::<CoreAgency>();
+    assert_nameable::<DispatchingModule>();
+    assert_nameable::<GoalBlockedReasonKind>();
+    assert_nameable::<SessionFailureCause>();
+    assert_nameable::<SessionLifecycleState>();
+    assert_nameable::<SessionParkCause>();
+    assert_nameable::<SessionParkResponder>();
+    assert_nameable::<SessionRecoveryOperation>();
+    assert_nameable::<SessionRetirementCause>();
+    assert_nameable::<SessionRetryableCause>();
+    assert_nameable::<SessionStructuralCause>();
+    assert_nameable::<SessionTerminalOutcome>();
+    assert_nameable::<SessionWait>();
+    assert_nameable::<ToolRequestId>();
+}
